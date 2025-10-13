92e25d21c6c30cec3896dd6dc91550a33eb18872
augusto-amado <augustoc.amado@gmail.com>
2025-10-13 15:16:40 -0300

    frontend/src/app/layout.tsx

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import { AuthProvider } from '@/contexts/AuthContext'
+import './globals.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  )
+}
